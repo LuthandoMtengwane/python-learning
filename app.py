492a3277ec2b8ff403ee2e87763c9a057d9c55a5
Luthando Mtengwane <luthandolindokuhle17@gmail.com>
2022-03-18 00:26:14 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+# First print
+print('hello world')
+
+# Variable declarations
+
+name = 'Luthando' #string
+
+age = 21 #number
+
+currency = 15.5 #float
+
+logged_in = True # boolean
+
+#strings
+
+firstName = 'Luthando'
+lastName = 'Mtengwane'
+
+greeting = f"Hello my name is {firstName} and my last name is {lastName}"
+
+print(len(greeting)) #checks the number of characters in greeting
